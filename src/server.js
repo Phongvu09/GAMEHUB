50b6b39e52b83a32bd42498edd31db3849ec5512
Tên Của Bạn <vuphong28102009@gmail.com>
2025-07-10 04:56:50 +0700

    update api.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
-// server.js
 import express from 'express';
 import fetch from 'node-fetch';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(cors());
 
 app.get('/api/steam/:appid', async (req, res) => {
     const appid = req.params.appid;
